bc0e3711ab6b766205eee19f309c72492733c973
Uriel Ofir <urielofir@gmail.com>
2023-09-05 14:10:32 +0300

    fix edit link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,14 +42,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.dev/UrielOfir/my-website',
+            'https://github.com/UrielOfir/my-website/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.dev/UrielOfir/my-website',
+            'https://github.com/UrielOfir/my-website/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
